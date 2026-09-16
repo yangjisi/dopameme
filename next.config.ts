b74f1cc1d74bcdfff9c898277bdcca528cdd01b5
yjs <yjs@demasian-ui-MacBookPro-2.local>
2026-09-16 12:21:10 +0900

    disable type check for prototype

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['172.30.1.29'], 
+  allowedDevOrigins: ['172.30.1.29'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
